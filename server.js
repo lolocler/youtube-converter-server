605d2482a0d7645cce1505120adf5cd8d8300d37
Laurent Clermont <laurentclermontfilm@gmail.com>
2025-04-07 12:28:36 +0200

    Update server.js to use dynamic PORT for Render

--- server.js
+++ server.js
@@ -80,6 +80,7 @@ const server = http.createServer(async (req, res) => {
     });
 });
 
-server.listen(8080, () => {
-    console.log('Server running at http://localhost:8080');
+const port = process.env.PORT || 8080;
+server.listen(port, () => {
+    console.log(`Server running at port ${port}`);
 });
